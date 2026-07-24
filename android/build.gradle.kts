1cf2e748ba9ebd18312b7897a16651b5e0138fd8
viktorh592-wq <viktorh592@gmail.com>
2026-07-24 15:20:53 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,17 +5,19 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.name = "pokatuha"
+// Стандартная настройка директории сборки для Flutter
+val newBuildDir: org.gradle.api.file.Directory = rootProject.layout.buildDirectory.dir("../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    val newSubprojectBuildDir: org.gradle.api.file.Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
-tasks.register<Delete>("clean") {
+tasks.register<org.gradle.api.tasks.Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
