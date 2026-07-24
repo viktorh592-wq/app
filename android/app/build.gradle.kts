669605dec6df384c1f4b7fd21c538959325eed31
viktorh592-wq <viktorh592@gmail.com>
2026-07-24 17:02:27 +0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 }
 
 android {
-    namespace = "com.example.pokatuha"
+    namespace = "com.pokatuha.app" // <-- ИСПРАВЛЕНО
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973" // ИСПРАВЛЕНИЕ 1: Актуальная версия NDK
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
-        isCoreLibraryDesugaringEnabled = true // ИСПРАВЛЕНИЕ 2: Включаем desugaring
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
@@ -21,7 +21,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.pokatuha"
+        applicationId = "com.pokatuha.app" // <-- ИСПРАВЛЕНО
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -40,6 +40,5 @@ flutter {
 }
 
 dependencies {
-    // ИСПРАВЛЕНИЕ 2: Добавляем библиотеку для desugaring
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
