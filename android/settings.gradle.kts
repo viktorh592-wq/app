53549c5297ed5f6ff5ccff2d27e417c7d704ccc8
viktorh592-wq <viktorh592@gmail.com>
2026-08-13 17:40:51 +0300

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,11 +1,11 @@
 pluginManagement {
-    def flutterSdkPath = {
-        def properties = new Properties()
-        file("local.properties").withInputStream { properties.load(it) }
-        def flutterSdkPath = properties.getProperty("flutter.sdk")
-        assert flutterSdkPath != null, "flutter.sdk not set in local.properties"
-        return flutterSdkPath
-    }()
+    val flutterSdkPath: String = run {
+        val properties = java.util.Properties()
+        file("local.properties").inputStream().use { properties.load(it) }
+        val flutterPath = properties.getProperty("flutter.sdk")
+        require(!flutterPath.isNullOrEmpty()) { "flutter.sdk not set in local.properties" }
+        flutterPath
+    }
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
@@ -17,10 +17,10 @@ pluginManagement {
 }
 
 plugins {
-    id "dev.flutter.flutter-plugin-loader" version "1.0.0"
-    id "com.android.application" version "8.7.0" apply false
-    id "org.jetbrains.kotlin.android" version "2.0.21" apply false
-    id "com.google.gms.google-services" version "4.4.2" apply false
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
-include ":app"
+include(":app")
