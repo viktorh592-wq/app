ff4f25f850dd85c5ceb37d4d91fdd78aaeaa7f23
viktorh592-wq <viktorh592@gmail.com>
2026-08-13 14:48:46 +0300

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,12 +1,19 @@
 pluginManagement {
     val flutterSdkPath = run {
         val properties = java.util.Properties()
-        file("local.properties").inputStream().use { properties.load(it) }
+        val localPropertiesFile = file("local.properties")
+        
+        // Безопасная проверка: файл может отсутствовать при первой сборке на CI/CD
+        if (localPropertiesFile.exists()) {
+            localPropertiesFile.inputStream().use { properties.load(it) }
+        }
+        
         val flutterSdkPath = properties.getProperty("flutter.sdk")
         require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
         flutterSdkPath
     }
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
     repositories {
         google()
         mavenCentral()
@@ -16,8 +23,8 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+    id("com.android.application") version "8.7.3" apply false // Актуальная стабильная версия
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false // Стабильная версия Kotlin
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
