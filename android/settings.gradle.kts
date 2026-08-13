fd6d2b8b9899a2a57048082358872993bc70b952
viktorh592-wq <viktorh592@gmail.com>
2026-08-13 15:10:11 +0300

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,17 +1,12 @@
 pluginManagement {
-    val flutterSdkPath = run {
-        val properties = java.util.Properties()
-        val localPropertiesFile = file("local.properties")
-        
-        // Безопасная проверка: файл может отсутствовать при первой сборке на CI/CD
-        if (localPropertiesFile.exists()) {
-            localPropertiesFile.inputStream().use { properties.load(it) }
-        }
-        
-        val flutterSdkPath = properties.getProperty("flutter.sdk")
-        require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-        flutterSdkPath
-    }
+    def flutterSdkPath = {
+        def properties = new Properties()
+        file("local.properties").withInputStream { properties.load(it) }
+        def flutterSdkPath = properties.getProperty("flutter.sdk")
+        assert flutterSdkPath != null, "flutter.sdk not set in local.properties"
+        return flutterSdkPath
+    }()
+
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
     repositories {
@@ -22,10 +17,10 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.7.3" apply false // Актуальная стабильная версия
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false // Стабильная версия Kotlin
-    id("com.google.gms.google-services") version "4.4.2" apply false
+    id "dev.flutter.flutter-plugin-loader" version "1.0.0"
+    id "com.android.application" version "8.10.0" apply false
+    id "org.jetbrains.kotlin.android" version "2.1.20" apply false
+    id "com.google.gms.google-services" version "4.4.2" apply false
 }
 
-include(":app")
+include ":app"
